c02d539434b3c44903fe6f0ad0c5eea7f2a32616
Cphayim <admin@cphayim.me>
2017-12-27 17:03:31 +0800

    feat: 添加 home / play 路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HomePage from '@/pages/HomePage'
+import PlayPage from '@/pages/PlayPage'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: HomePage
+    },
+    {
+      path: '/play',
+      name: 'play',
+      component: PlayPage
     }
   ]
 })
