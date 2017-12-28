4ac29dc1ba525738664512e94b7478fe580df9c3
Cphayim <admin@cphayim.me>
2017-12-28 23:25:29 +0800

    opt: 添加 state

--- src/store/store.js
+++ src/store/store.js
@@ -2,24 +2,38 @@
  * @Author: Cphayim 
  * @Date: 2017-12-27 10:39:19 
  * @Last Modified by: Cphayim
- * @Last Modified time: 2017-12-27 13:40:23
+ * @Last Modified time: 2017-12-27 17:15:46
  */
 import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+
+const gameConfig = window.gameConfig
+
 /**
  * 全局状态管理
  */
-
 export default new Vuex.Store({
   state: {
     /**
+     * 是否显示 loading
      * @property {boolean} isShowLoading
      */
-    isShowLoading: true
+    isShowLoading: true,
 
+    /**
+     * 是否登记过
+     * @property {boolean} isRegistered
+     */
+    isRegistered: gameConfig.isRegistered,
+
+    /**
+     * 是否能开始游戏
+     * @property {boolean} canPlay
+     */
+    canPlay: gameConfig.canPlay
   },
   mutations: {
     /**
