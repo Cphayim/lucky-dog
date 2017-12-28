53e30702525bafdd3c04ad78b775ec21eaebe759
Cphayim <admin@cphayim.me>
2017-12-28 23:19:28 +0800

    opt: add axios

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import '@/styles/common.scss' // 2
 import Response from '@/scripts/response'
 import Vue from 'vue'
 import App from '@/App'
+import axios from 'axios'
 import router from '@/router'
 import store from '@/store/store'
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
+Vue.prototype.$axios = axios
 
 /* eslint-disable no-new */
 new Vue({
