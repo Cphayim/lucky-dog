f92ba70f8832fd486e2fec47a101ceb710231180
Cphayim <admin@cphayim.me>
2017-12-28 23:24:54 +0800

    feat: 添加 首页横幅和窗帘组件

---
+++ src/components/Banner.vue
@@ -0,0 +1,117 @@
+<!-- 
+  横幅组件 
+  参数说明：
+-->
+<template>
+  <transition name="banner-slide">
+    <div class="m-banner" >
+      <!-- 默认 -->
+      <div v-if="!egg" class="item">
+        <!-- 主标题 -->
+        <div class="main-title"></div>
+        <!-- /主标题 -->
+        <!-- 副标题盒子 -->
+        <div class="sub-title-box">
+          <div class="sub-title"></div>
+        </div>
+        <!-- /副标题盒子 -->
+      </div>
+      <!-- /默认 -->
+
+      <!-- 彩蛋 -->
+      <div v-else class="item">
+        <!-- 主标题 -->
+        <div class="main-title main-title-2"></div>
+        <!-- /主标题 -->
+        <!-- 副标题盒子 -->
+        <div class="sub-title-box">
+          <div class="sub-title sub-title-2"></div>
+        </div>
+        <!-- /副标题盒子 -->
+      </div>
+      <!-- /彩蛋 -->
+    </div>  
+  </transition>
+</template>
+
+<script>
+/*
+ * @Author: Cphayim 
+ * @Date: 2017-12-27 11:23:05 
+ * @Last Modified by: Cphayim
+ * @Last Modified time: 2017-12-28 15:19:19
+ */
+
+export default {
+  name: 'Banner',
+  props: {
+    egg: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.m-banner {
+  pointer-events: none;
+  position: absolute;
+  top: 0;
+  z-index: 15;
+  width: 100%;
+  padding-top: r(10);
+
+  .item {
+    position: absolute;
+    top: r(15);
+    width: 100%;
+    animation: banner-float 1s linear alternate infinite both;
+  }
+
+  .main-title,
+  .sub-title-box {
+    position: relative;
+    margin: 0 auto;
+    width: r(350);
+  }
+  .main-title {
+    z-index: 2;
+    height: r(105.5);
+    @include image("../assets/img/home-main-title.png");
+  }
+  .main-title-2 {
+    background-image: url("../assets/img/home-main-title-2.png");
+  }
+  .sub-title-box {
+    transform: translateY(r(-40));
+  }
+  .sub-title {
+    height: r(92.5);
+    @include image("../assets/img/home-sub-title.png");
+  }
+  .sub-title-2 {
+    background-image: url("../assets/img/home-sub-title-2.png");
+  }
+}
+
+.banner-slide-enter-active,
+.banner-slide-leave-active {
+  transition: all 0.4s 0.4s;
+}
+
+.banner-slide-enter,
+.banner-slide-leave-to {
+  transform: translateY(r(-100));
+  opacity: 0;
+}
+
+@keyframes banner-float {
+  from {
+    transform: translateY(10);
+  }
+  to {
+    transform: translateY(r(-10));
+  }
+}
+</style>
---
+++ src/components/Curtain.vue
@@ -0,0 +1,72 @@
+<!-- 
+  窗帘组件 
+-->
+<template>
+  <transition name="curtain-slide" :duration="400">
+    <div class="m-curtain">
+      <!-- 左侧窗帘 -->
+      <div class="curtain-l"></div>
+      <!-- /左侧窗帘 -->
+
+      <!-- 右侧窗帘 -->
+      <div class="curtain-r"></div>
+      <!-- /右侧窗帘 -->
+    </div> 
+  </transition> 
+</template>
+
+<script>
+/*
+ * @Author: Cphayim 
+ * @Date: 2017-12-27 11:23:05 
+ * @Last Modified by: Cphayim
+ * @Last Modified time: 2017-12-28 15:08:38
+ */
+
+export default {
+  name: 'Curtain'
+}
+</script>
+
+<style lang="scss" scoped>
+.m-curtain {
+  pointer-events: none;
+  position: absolute;
+  top: 0;
+  z-index: 10;
+  bottom: 20%;
+  width: 100%;
+  .curtain-l,
+  .curtain-r {
+    position: absolute;
+    top: 0;
+    width: 50%;
+    height: 100%;
+  }
+  .curtain-l {
+    left: 0;
+    @include image("../assets/img/home-curtain-l.png");
+  }
+  .curtain-r {
+    right: 0;
+    @include image("../assets/img/home-curtain-r.png");
+  }
+}
+
+.curtain-slide-enter-active,
+.curtain-slide-leave-active {
+  .curtain-l,
+  .curtain-r {
+    transition: all 0.4s;
+  }
+}
+.curtain-slide-enter,
+.curtain-slide-leave-to {
+  .curtain-l {
+    transform: translateX(-100%);
+  }
+  .curtain-r {
+    transform: translateX(100%);
+  }
+}
+</style>
