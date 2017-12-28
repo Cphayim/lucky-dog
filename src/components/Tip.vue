5c044a5281d7b990d4bc5f7a20794854806cbfd2
Cphayim <admin@cphayim.me>
2017-12-28 23:25:57 +0800

    feat: 添加 提示层组件

---
+++ src/components/Tip.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="m-tip">
+    <span>{{content}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tip',
+  props: {
+    content: {
+      type: String,
+      default: '提示内容'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.m-tip {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 900;
+  width: 100%;
+  line-height: 40px;
+  text-align: center;
+  background-color: rgba($color_f1, 0.7);
+  @include font($font_small, #fff);
+}
+</style>
